c3ed67a1f759cc5fa7126da55a8898734698ee77
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-09-15 18:19:00 -0400

    Update enemy_AI.py

--- STP2/enemy_AI.py
+++ STP2/enemy_AI.py
@@ -1,13 +1,30 @@
 from enemy_intent import EnemyIntent 
 import random
+import buff_type
 
 class EnemyAI:
     def __init__(self):
         pass
 
+    # place holder AI: make 3 kinds of decison randomly 
     def make_intent(self)->EnemyIntent:
         intent = EnemyIntent()
-        intent.is_attack = True
-        intent.attack_value = random.randint(1,20)
-        print("BOSS intent: -[attack]-",intent.attack_value)
+        intent_type_rand = random.random()
+        if intent_type_rand < 0.33:
+            # attack intent
+            intent.is_attack = True
+            intent.attack_value = random.randint(1,20)
+            print("BOSS intent: -[attack]-",intent.attack_value)
+        elif intent_type_rand < 0.66:
+            # debuff intent
+            intent.is_debuff = True
+            intent.debuff_type = buff_type.BuffType.Weakened
+            intent.debuff_value = 1
+            print("BOSS intent: -[debuff]-",intent.debuff_type)
+        else:
+            # enbuff intent
+            intent.is_enbuff = True
+            intent.enbuff_type = buff_type.BuffType.Artifact
+            intent.enbuff_value = 1
+            print("BOSS intent: -[enbuff]-",intent.enbuff_type)
         return intent
