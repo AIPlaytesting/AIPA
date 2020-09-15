257de73473f733c3f8596b9c74fbacc44eceb0a3
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-09-15 17:29:41 -0400

    add game runner

--- STP2/gameManager.py
+++ STP2/gameManager.py
@@ -1,17 +1,29 @@
 from STP2.combatUnit import CombatUnit
-
+from STP2.enemyIntent import EnemyIntent
 class GameState:
     def __init__(self):
-        # placehold: hp value all 100
-        self.player = CombatUnit(100)
-        self.boss = CombatUnit(100)
+        self.player = CombatUnit(100) # placehold: hp value all 100
+        self.boss = CombatUnit(100) # placehold: hp value all 100
+        self.currentBossIntent = EnemyIntent()
         # self.deck = Deck()
 
 class GameManager:
     def __init__(self):
         self.gameState = GameState()
 
-def init_game():
-    gameManager.gameState = GameState()
+    def init_game(self):
+        gameManager.gameState = GameState()
+
+    def is_game_end(self):
+        return self.gameState.player.currentHP <= 0 or self.gameState.boss.currentHP < 0
+
+    def start_turn(self):
+        pass
+
+    def get_player_action_space(self):
+        pass
+
+    def execute_enemy_intent(self):
+        pass
 
 gameManager = GameManager()
---
+++ STP2/gameRunner.py
@@ -0,0 +1,15 @@
+from STP2.gameManager import GameManager
+
+game_manager = GameManager()
+game_manager.init_game()
+while(not game_manager.is_game_end()):
+    # init for game start
+    game_manager.start_turn()
+    # generate action space
+    action_space = game_manager.get_player_action_space()
+    # wait for player input
+
+    # apply player action
+
+    # apply BOSS intent
+    game_manager.execute_enemy_intent()
