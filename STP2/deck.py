067748796402bec7b2ba85061389a0ee9a22b327
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-09-15 20:54:38 -0400

    Create deck.py

---
+++ STP2/deck.py
@@ -0,0 +1,19 @@
+import STP2.Pile
+class Deck:
+    def __init__(self):
+        super().__init__()
+    
+    def load_from_file(self,directory):
+        pass
+
+    def get_cards_on_hand(self):
+        pass
+    
+    def draw_cards_to_hand(self,num):
+        pass
+    
+    def exhaust_card(self,card_to_exhaust):
+        pass
+
+    def discard_card(self,card_to_discard):
+        pass
