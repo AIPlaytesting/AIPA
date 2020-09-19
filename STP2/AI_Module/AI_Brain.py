cf792da0a5956b64c6a7d248db21bbaa343dd5dc
Ram Iyer <lordram17@yahoo.co.in>
2020-09-18 22:16:57 -0400

    Added functions to initialize AI Brain and sample from Replay Buffer

--- STP2/AI_Module/AI_Brain.py
+++ STP2/AI_Module/AI_Brain.py
@@ -1,10 +1,76 @@
 import tensorflow as tf
+from tensorflow import keras
+import ReplayBuffer
+import numpy as np
+
+
+def BuildDeepQNetwork(state_space_dim, action_space_dim, hl1_dim, hl2_dim, hl3_dim, hl4_dim):
+    model = keras.Sequential(
+        keras.layers.Dense(state_space_dim, activation='relu'),
+        keras.layers.Dense(hl1_dim, activation='relu'),
+        keras.layers.Dense(hl2_dim, activation='relu'),
+        keras.layers.Dense(hl3_dim, activation='relu'),
+        keras.layers.Dense(hl4_dim, activation='relu'),
+        #Dont want any activation for the output layer since it is the Q-value
+        keras.layers.Dense(action_space_dim, activation=None)
+    )
+
+    model.compile(optimizer='Adam', loss='mean_squared_error', metrics=['accuracy', 'mean_squared_error'])
+
+    return model
 
 class AI_Brain:
     
-    def __init__(self):
-        pass
+    def __init__(self, gamma,
+        state_space_dim, action_space_dim, hidden_layer_dims,
+        epsilon, epsilon_dec, epsilon_min, 
+        mem_size, batch_size):
+
+        self.replay_buffer = ReplayBuffer.ReplayBuffer(state_space_dim, mem_size)
+        self.action_space = np.arange(action_space_dim)
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_dec = epsilon_dec
+        self.epsilon_min = epsilon_min
+        self.mem_size = mem_size
+        self.batch_size = batch_size
+
+        self.q_model = BuildDeepQNetwork(
+            state_space_dim=state_space_dim,
+            action_space_dim=action_space_dim,
+            hl1_dim=hidden_layer_dims[0],
+            hl2_dim = hidden_layer_dims[1],
+            hl3_dim = hidden_layer_dims[2],
+            hl4_dim = hidden_layer_dims[3]
+        )
+
+
+    
 
     def PredictAction(self, state):
-        return None
+        
+        if (np.random.random() < self.epsilon) :
+            action = np.random.choice(self.action_space)
+        else:
+            state = np.array([state])
+            actions = self.q_model.predict(state)
+            action = np.argmax(actions)
+        
+        return action
+
+
+    def Learn(self):
+        
+        if self.replay_buffer.mem_ctr < self.batch_size :
+            return
+        
+        states, new_states, rewards, actions, terminals = self.replay_buffer.SampleBuffer(self.batch_size)
+
+
+
+
+    def StoreTransition(self, state, new_state, action, reward, isTerminal):
+        self.replay_buffer.StoreTransition(state, new_state, action, reward, isTerminal)
+
+
     
--- STP2/AI_Module/ReplayBuffer.py
+++ STP2/AI_Module/ReplayBuffer.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class ReplayBuffer:
     
-    def __init__(self, state_space_dim, action_space_dim, mem_size):
+    def __init__(self, state_space_dim, mem_size):
         self.mem_size = mem_size
         self.mem_ctr = 0
 
@@ -26,9 +26,16 @@ class ReplayBuffer:
         self.terminal_memory[index] = isTerminal
     
     def SampleBuffer(self, batch_size):
-        pass
+        max_mem = min(self.mem_ctr, self.mem_size)
 
+        #np.random.choice below returns an array of size batch_size. Contains elements from 0 - max_mem. No replacement
+        batch = np.random.choice(max_mem, batch_size, replace=False)
 
+        states = self.state_memory[batch]
+        new_states = self.new_state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        terminals = self.terminal_memory[batch]
 
-    
+        return states, new_states, actions, rewards, terminals
 
