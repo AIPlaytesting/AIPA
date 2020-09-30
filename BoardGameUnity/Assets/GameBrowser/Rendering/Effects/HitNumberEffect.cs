1fc7c01da5bcf23b832c44fa3da6bfd6b45c3433
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-09-30 12:40:02 -0400

    add prefabs

--- BoardGameUnity/Assets/GameBrowser/Rendering/Effects/HitNumberEffect.cs
+++ BoardGameUnity/Assets/GameBrowser/Rendering/Effects/HitNumberEffect.cs
@@ -12,8 +12,7 @@ namespace GameBrowser.Rendering {
         public override void Play(bool destroyOnComplete = true) {
             float duration = 2.5f;
             numberText.text = hitValue.ToString();
-            numberText.DOFade(0,duration);
-            numberText.transform.DOShakeScale(duration);
+            numberText.transform.DOShakeScale(duration/2).onComplete = ()=> { numberText.DOFade(0, duration/2); };
             if (destroyOnComplete) {
                 Destroy(gameObject, duration + 0.1f);
             }
