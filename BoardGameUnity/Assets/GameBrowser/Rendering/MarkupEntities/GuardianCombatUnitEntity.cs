c2dbbb6f19fa8c582269947ba4e38f646197a25a
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-10-07 18:50:19 -0400

    show boss switch mode in Unity

---
+++ BoardGameUnity/Assets/GameBrowser/Rendering/MarkupEntities/GuardianCombatUnitEntity.cs
@@ -0,0 +1,28 @@
+﻿using GameBrowser.Rendering;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameBrowser {
+    /// <summary>
+    /// Guardian is one of the BOSS in the game
+    /// </summary>
+    public class GuardianCombatUnitEntity : CombatUnitEntity {
+        public Image bossImage;
+        public Sprite offensiveModeSprite;
+        public Sprite defensiveModeSprite;
+
+        public override void HookTo(Markup markup) {
+            base.HookTo(markup);
+            var combatUnitMarkup = hookedMarkup as CombatUnitMarkup;
+            string mode = combatUnitMarkup.information["mode"];
+            if (mode == "Offensive") {
+                bossImage.sprite = offensiveModeSprite;
+            }
+            else {
+                bossImage.sprite = defensiveModeSprite;
+            }
+        }
+    }
+}
