705a749b3c92d909779b1d50bfd30e9bbeab9ff2
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-10-06 20:52:26 -0400

    change card interaction to drag

--- BoardGameUnity/Assets/GameBrowser/Rendering/MarkupEntities/SelectableCardEntity.cs
+++ BoardGameUnity/Assets/GameBrowser/Rendering/MarkupEntities/SelectableCardEntity.cs
@@ -9,8 +9,12 @@ namespace GameBrowser.Rendering {
         public GameObject glow;
         public TextMeshPro cardName;
         public TextMeshPro energy;
-        
+        public PlayCardInputTrigger playCardInputTrigger;
+
+        private bool isSelectable = true;
         private bool isSelected = false;
+        private bool isDraged = false;
+        private Vector3 initPosition;
 
         public override void HookTo(Markup markup) {
             base.HookTo(markup);
@@ -18,17 +22,46 @@ namespace GameBrowser.Rendering {
             tooltipText = "description: "+ hookedCardMarkup.name;
             cardName.text = hookedCardMarkup.name;
             energy.text = hookedCardMarkup.energyCost.ToString();
+
+            // set input trigger
+            playCardInputTrigger.cardName = hookedCardMarkup.name;
+            playCardInputTrigger.cardGUID = hookedCardMarkup.gameUniqueID;
         }
 
         private void Awake() {
             base.Awake();
             isSelected = false;
+            initPosition = transform.position;
             OnIsSelectedChange();
         }
 
         private void OnMouseDown() {
+            if (!isSelectable) {
+                return;
+            }
+
             isSelected = !isSelected;
+            initPosition = transform.position;
+            isDraged = true;
             OnIsSelectedChange();
+        }
+
+        private void OnMouseUp() {
+            isDraged = false;
+            //transform.position = initPosition;
+            isSelectable = false;
+            playCardInputTrigger.TriggerUserInput();
+        }
+
+        private void Update() {
+            if (!isSelectable) {
+                return;
+            }
+
+            if (isDraged) {
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                transform.position = new Vector3( worldPosition.x,worldPosition.y,transform.position.z);
+            }
         }
 
         private void OnIsSelectedChange() {
