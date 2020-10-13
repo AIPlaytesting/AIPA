3ead1d641e8adb21fbedca84e73e127843bb59ad
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-10-13 10:13:13 -0400

    add renderclass into value markup

--- BoardGameUnity/Assets/GameBrowser/MarkupLanguage/ValueMarkup.cs
+++ BoardGameUnity/Assets/GameBrowser/MarkupLanguage/ValueMarkup.cs
@@ -6,8 +6,14 @@ namespace GameBrowser {
     [System.Serializable]
     public class ValueMarkup :Markup
     {
+        public const string RENDER_CLASS_ENERGY= "energy";
+        public const string RENDER_CLASS_GUADIAN = "guadian";
+        public const string RENDER_CLASS_RLREWARD = "rlreward";
+
         public string name = "value";
         public float curValue = 0;
         public float maxValue = 3;
+        // value class is used for telling renderer how to render this value
+        public string renderClass = "";
     }
 }
--- STP2/protocol.py
+++ STP2/protocol.py
@@ -84,11 +84,12 @@ class MarkupFactory:
         markup['cardsOnHand'] = create_cards_markup_by_card_instances(game_state.deck.get_card_instances_on_hand())
         markup['drawPile'] = create_cards_markup_by_card_instances(game_state.deck.get_draw_pile().cards)
         markup['discardPile'] = create_cards_markup_by_card_instances(game_state.deck.get_discard_pile().cards)
-        markup['energy'] = cls.create_value_markup('energy',game_state.player_energy,3)
+        markup['energy'] = cls.create_value_markup('energy',game_state.player_energy,3,'energy')
         markup['guadianModeValue'] = cls.create_value_markup(
             'guadianModeValue',
             game_state.boss_AI.accumulator,
-            game_state.boss_AI.transformTriggerPoint)
+            game_state.boss_AI.transformTriggerPoint,
+            'guadian')
         return markup
 
     @classmethod
@@ -125,11 +126,12 @@ class MarkupFactory:
         return markup
 
     @classmethod
-    def create_value_markup(cls,value_name,cur_value,max_value):
+    def create_value_markup(cls,value_name,cur_value,max_value,renderclass):
         markup = {}
         markup['name'] = value_name
         markup['curValue'] = cur_value
         markup['maxValue'] = max_value
+        markup['renderClass'] = renderclass
         return markup
 
     @classmethod
