5a37854e65defe232aecd4e0c9e6de1cfbda659a
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-10-13 17:11:17 -0400

    add cards info in modifier UI

---
+++ BoardGameUnity/Assets/Scripts/RuntimeModifier/CardModifyEntry.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GameBrowser;
+using TMPro;
+
+public class CardModifyEntry : MonoBehaviour
+{
+    public Image cardImg;
+    public TextMeshProUGUI nameText;
+
+    private Sprite cardSprite = null;
+    private WWW spriteWWW;
+
+    private void Update() {
+        if (spriteWWW != null && spriteWWW.isDone && cardSprite == null) {
+            var srcTex = spriteWWW.texture;
+            if (srcTex) {
+                cardSprite = Sprite.Create(srcTex, new Rect(0, 0, srcTex.width, srcTex.height), Vector2.zero);
+            }
+            else {
+                spriteWWW = null;
+            }
+            cardImg.sprite = cardSprite;
+        }
+    }
+
+    public void HookModifyTarget(CardMarkup cardMarkup) {
+        nameText.text = cardMarkup.name;
+        spriteWWW = new WWW(cardMarkup.imgAbsPath);
+    }
+}
--- BoardGameUnity/Assets/Scripts/RuntimeModifier/CardsOnHandModifyPage.cs
+++ BoardGameUnity/Assets/Scripts/RuntimeModifier/CardsOnHandModifyPage.cs
@@ -5,7 +5,29 @@ using GameBrowser;
 
 public class CardsOnHandModifyPage : MonoBehaviour
 {
-    public void HookModifyTarget(CardMarkup[] cardsOnHand) { 
-    
+    [SerializeField]
+    GameObject cardEntryPrefab;
+    [SerializeField]
+    Transform cardEntryRoot;
+
+    private List<CardModifyEntry> currentCardEntries = new List<CardModifyEntry>();
+
+
+    public void HookModifyTarget(CardMarkup[] cardsOnHand) {
+        foreach (var cardEntry in currentCardEntries) {
+            GameObject.DestroyImmediate(cardEntry.gameObject);
+        }
+        currentCardEntries.Clear();
+
+        foreach (var cardmarkup in cardsOnHand) {
+            var GO = Instantiate(cardEntryPrefab);
+            GO.transform.SetParent(cardEntryRoot);
+            GO.transform.localScale = Vector3.one;
+            GO.name = "buff entry: " + cardmarkup.name;
+            var buffEntry = GO.GetComponent<CardModifyEntry>();
+            buffEntry.HookModifyTarget(cardmarkup);
+            currentCardEntries.Add(buffEntry);
+        }
+
     }
 }
