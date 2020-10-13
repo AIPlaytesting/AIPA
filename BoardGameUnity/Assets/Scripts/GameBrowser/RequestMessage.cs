35ebdf7f4db9275123121b01bdfa447d0304fcb4
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-10-13 19:56:47 -0400

    add request type field in request message

--- BoardGameUnity/Assets/Scripts/GameBrowser/RequestMessage.cs
+++ BoardGameUnity/Assets/Scripts/GameBrowser/RequestMessage.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 namespace GameBrowser {
     [System.Serializable]
     public class RequestMessage {
+        public enum Type { 
+            None,
+            UserInput,
+            DBQuery
+        }
+
+        public Type requestType = Type.None;
         public UserInput userInput = null;
 
         public RequestMessage(UserInput userInput) {
+            this.requestType = Type.UserInput;
             this.userInput = userInput;
         }
     }
