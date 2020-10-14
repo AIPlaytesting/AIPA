67e86ae44cc17fda284d1858b0c854c74e28d48e
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-10-13 21:27:36 -0400

    create main classes of backend

---
+++ STP2/backend/connection.py
@@ -0,0 +1,12 @@
+class Connection:
+    def __init__(self):
+        self.message
+
+    def connect(self):
+        pass
+
+    def wait_one_request(self,request_type):
+        pass
+
+    def send_response(self,response):
+        pass
---
+++ STP2/backend_launcher.py
@@ -0,0 +1,23 @@
+from backend.dbqueryloop import DBQueryLoop
+from backend.gameplayloop import GameplayLoop
+from backend.connection import Connection
+from db.game_database import GameDatabase, calculate_root_dir
+from game_manager import GameManager
+
+# connect to frontend
+connection = Connection()
+connection.connect()
+
+# load database
+db_root = calculate_root_dir()
+game_db = GameDatabase(db_root)
+
+# create game manager
+game_manager = GameManager(game_db.game_app_data)
+
+# start loops
+gameplayloop = GameplayLoop(connection,game_manager)
+gameplayloop.run()
+
+dbqueryloop = DBQueryLoop(connection,game_manager)
+dbqueryloop.run()
