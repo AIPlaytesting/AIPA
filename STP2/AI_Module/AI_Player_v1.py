508a5342609634139eb02b872fd41cc7c4da003a
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-10-15 18:31:51 -0400

    meeting changes

--- STP2/AI_Module/AI_Player_v1.py
+++ STP2/AI_Module/AI_Player_v1.py
@@ -3,12 +3,12 @@ import tensorflow as tf
 from tensorflow import keras
 import AI_Module.AI_Transformer
 import game_manager
-
+import numpy as np
 
 class AI_Player:
 
     def __init__(self, game_manager: game_manager.GameManager = None):
-        self.LoadModel('14oct_save_model_test')
+        self.LoadModel('14-Oct-20 21-22 saved-model')
         self.ai_transformer = AI_Module.AI_Transformer.AI_Transformer()
         self.state_space = self.ai_transformer.state_space
         self.action_space = self.ai_transformer.action_space
@@ -18,9 +18,9 @@ class AI_Player:
     def LoadModel(self, model_folder_name=''):
 
         if model_folder_name == '':
-            model_folder_name = '14oct_save_model_test'
+            model_folder_name = '14-Oct-20 21-22 saved-model'
 
-        model_path = 'Saved Models\\' + model_folder_name
+        model_path = 'Shared Models\\' + model_folder_name
         self.q_model = tf.keras.models.load_model(model_path)
         self.q_model.summary()
         print("Model Loaded at Path -  " + model_path)
@@ -29,9 +29,10 @@ class AI_Player:
 
     #Communicate with the AI_Transformer to get the lastest state
     #Use the state information to predict an action
-    def GetActionVector(self, current_game_state, playable_cards_list):
+    def GetActionVector(self):
         ai_state = self.ai_transformer.GetAIStateSpace(self.game_manager.game_state, self.game_manager.get_current_playable_cards())
-        action_vec = self.q_model.__call__(ai_state)
+        print(ai_state)
+        action_vec = self.q_model.__call__(np.array([ai_state], dtype = np.float))
 
         return action_vec
 
