b3c041b7db1e5bf562ac49493b7165f9af62adb1
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-10-16 17:19:38 -0400

    imp: add buffs in runtime modifier

---
+++ BoardGameUnity/Assets/Scripts/RuntimeModifier/BuffAddModifyEntry.cs
@@ -0,0 +1,43 @@
+﻿using GameBrowser;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffAddModifyEntry :DropdownSelectEntry
+{
+    public bool isModifyPlayer = false;
+
+    private BuffMarkup buffToAdd= new BuffMarkup();
+
+    protected override void Awake() {
+        base.Awake();
+        SetCurrentValue("select a buff");
+    }
+
+    protected override void OnDropdownValueChanged(string newValue) {
+        buffToAdd.buffName = newValue;
+        buffToAdd.buffValue = 1;
+    }
+
+    protected override string[] GetDropdownOptions() {
+        return GameRuntimeModifier.Instance.registeredBuffnames;
+    }
+
+    // TODO-BUG: handle case that add buff already exist
+    public void OnClickAddBuff() {
+        CombatUnitMarkup combatUnityModified = null;
+        if (isModifyPlayer) {
+            combatUnityModified = RuntimeModifierWindow.Instance.modifyTarget.player;
+        }
+        else {
+            combatUnityModified = RuntimeModifierWindow.Instance.modifyTarget.enemies[0];
+        }
+
+        var newBuffs = new List<BuffMarkup>();
+        newBuffs.Add(buffToAdd);
+        newBuffs.AddRange(combatUnityModified.buffs);
+        combatUnityModified.buffs = newBuffs.ToArray();
+
+        RuntimeModifierWindow.Instance.InformModificitonHappened();
+    }
+}
