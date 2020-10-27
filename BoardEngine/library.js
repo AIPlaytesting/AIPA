b96d4d7822252c453a3b322277052c362faebd38
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-10-27 18:12:55 -0400

    switch page when clike game button

--- BoardEngine/library.js
+++ BoardEngine/library.js
@@ -3,16 +3,20 @@ const dbmanager = require('./dbmanager.js');
 function libraryOnLoad(){
     let gameEntryList = $("#game-entry-list")
     let installedGames = dbmanager.getInstalledGameApps()
-    for(game of installedGames){
-        gameEntryList.append(createLibraryEntry(game))
+    for(gameName of installedGames){
+        gameEntryList.append(createLibraryEntry(gameName))
     }
+
     gameEntryList.append(createNewGameBtn())
+
+    updateGameMainPage(installedGames[0])
 }
 
 function createLibraryEntry(gameName){
     let entryBtn =$(document.createElement('button'))
     entryBtn.text(gameName)
     entryBtn.attr('class',"btn btn-light")
+    entryBtn.click(function(){updateGameMainPage(gameName)})
     return entryBtn
 }
 
@@ -21,4 +25,8 @@ function createNewGameBtn(){
     entryBtn.text("+New Game")
     entryBtn.attr('class',"btn btn-dark")
     return entryBtn
+}
+
+function updateGameMainPage(gameName){
+    $('#game-title').text(gameName)
 }
