a9bcee7a591417cbeacd7477b588349b7bb84901
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-10-27 19:30:39 -0400

    load all info into JS from DB

--- BoardEngine/dbmanager.js
+++ BoardEngine/dbmanager.js
@@ -1,3 +1,6 @@
+// for historical reason, the naming styple in db is same to the Python one
+// because the database is originnally wrote in Python
+
 const fs = require('fs');
 
 var dbRoot = '../STP2/DATA';
@@ -9,8 +12,6 @@ function loadDB(onFinishLoad){
         if (err) throw err;
         let manifest = JSON.parse(data);
         installedGames = manifest.installed_app;
-        console.log(manifest.installed_app)
-        console.log(installedGames)
         onFinishLoad()
     });     
 }
@@ -18,15 +19,40 @@ function loadDB(onFinishLoad){
 // return: gameData
 function loadGameData(gameName){
     let gameRoot = dbRoot +'\\'+gameName;
-    let gameData = {}
-    initData = fs.readFileSync(gameRoot+'\\init.json','utf8')
-    gameData.init = JSON.parse(initData)
+    let gameData = {};
+    // load init.json
+    initData = fs.readFileSync(gameRoot+'\\init.json','utf8');
+    gameData.init = JSON.parse(initData);
+
+    // load all decks
+    gameData.decks = {}
+    let deckFileRoot = gameRoot +'\\'+gameData.init.decks_directory;
+    // all decks are stored in ../DeckFileRoot/(deckname).json 
+    for(deckFile of fs.readdirSync(deckFileRoot,"utf8")){
+        deckData = fs.readFileSync(deckFileRoot+'\\'+deckFile,'utf8');
+        gameData.decks[deckFile] = JSON.parse(deckData)
+    }
+
+    // load all cards
+    gameData.cards = {}
+    let cardFileRoot = gameRoot +'\\'+gameData.init.cards_directory;
+    // all cards are stored in ../cardFileRoot/(cardname).json 
+    for(cardFile of fs.readdirSync(cardFileRoot,"utf8")){
+        cardData = fs.readFileSync(cardFileRoot+'\\'+cardFile,'utf8');
+        gameData.cards[cardFile] = JSON.parse(cardData)
+    }
+
+    // load gameData.buffInfo
+    let buffFilePath = gameRoot +'\\' + gameData.init.buffs_file;
+    gameData.buffInfo =JSON.parse(fs.readFileSync(buffFilePath))
+
+    // load gameData.rules
+    let ruleFilePath = gameRoot +'\\' + gameData.init.rules_file
+    gameData.rules = JSON.parse(fs.readFileSync(ruleFilePath))
     return gameData
 }
 
 function getInstalledGameApps(){
-    console.log("get called")
-    console.log(installedGames)
     return installedGames;
 }
 
