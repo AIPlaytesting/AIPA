e4eeb7ede8daa6c5f099c5d70123442b96206d26
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-11-06 11:39:52 -0500

    add pie chart into card relationship section

--- BoardEngine/scripts/aiPlaytesting.js
+++ BoardEngine/scripts/aiPlaytesting.js
@@ -28,22 +28,33 @@ function onFinishPlaytest(){
 
 function drawStatisticPage(){
     $('#data-page').text("")
-    drawStatisticCatagories()
-    drawBasicData()
+    drawBasicDataSection()
+    drawCardsDataSection()
+    drawCardRelationshipDataSection()
 }
 
-function drawStatisticCatagories(){
-    $('#data-tab-second').text("")
-    let catagories = ["Basic","Cards","CardRelationship"]
-    let clicks = [drawBasicData,drawCardsData,drawCardRelationshipData]
-    for(i = 0; i < catagories.length; i++){
-        let catagoryBtn = $(document.createElement('button')).attr('class','btn').text(catagories[i]).click(clicks[i])
-        $('#data-tab-second').append(catagoryBtn)
-    }
+function createSection(sectionName,sectionID){
+    let sectionDiv = $(document.createElement('div'))
+    .attr('class','container')
+    let collapseBtn = $(document.createElement('button'))
+    .attr('class','btn col-12')
+    .attr('data-toggle','collapse')
+    .attr('data-target','#'+sectionID)
+    .attr('style','background-color: #58635b;')
+    .text(sectionName)
+
+    let collapseDiv = $(document.createElement('div'))
+    .attr('id',sectionID)
+    .attr('class','collapse show')
+    sectionDiv.append(collapseBtn,collapseDiv)
+
+    return sectionDiv  
 }
 
-function drawBasicData(){
-    $('#data-page').text("")
+function drawBasicDataSection(){
+    let setionDiv = createSection('basic','basic-data-section')
+    $('#data-page').append(setionDiv)
+
     let winrateCard = $(document.createElement('div'))
     .attr('class','card bg-secondary text-white display-3')
     .text("winrate: 86.7%")
@@ -60,45 +71,46 @@ function drawBasicData(){
     .attr('class','card bg-info text-white display-4')
     .text("average Turn Count: 3.56")
 
-    $('#data-page').append(winrateCard,playerHPCard,bossHPCard,turnCountCard)
+    $('#basic-data-section').append(winrateCard,playerHPCard,bossHPCard,turnCountCard)
 }
 
-function drawCardsData(){
-    $('#data-page').text("")
-    dataVisualizer.drawRankChart('data-page')
+function drawCardsDataSection(){
+    let setionDiv = createSection('cards','cards-data-section')
+    $('#data-page').append(setionDiv)
+    dataVisualizer.drawRankChart('cards-data-section')
 }
 
-
-function drawCardRelationshipData(){
-    $('#data-page').text("")
-    dataVisualizer.drawRelationshipTable('data-page')
+function drawCardRelationshipDataSection(){
+    let setionDiv = createSection('cards relationship','cards-relatioship-data-section')
+    $('#data-page').append(setionDiv)
+    let tableDiv = $(document.createElement('div')).attr('id','card-relationship-table')
+    let pieDiv = $(document.createElement('div')).attr('id','card-relationship-pie')
+    $('#cards-relatioship-data-section').append(tableDiv,pieDiv)
+    dataVisualizer.drawRelationshipTable('card-relationship-table')
+    let data = {a: 9, b: 20, c:30, d:8, e:12}
+    dataVisualizer.drawPieChart('card-relationship-pie',data)
+    
 }
-
-
 
 function drawAnomaliesPage(){
     $('#data-page').text("")
-    drawAnomaliesCatagories()
+    drawAnomaliesSection('FastWin')
+    drawAnomaliesSection('MaxDamage')
+    drawAnomaliesSection('LongestGame')
 }
 
-function drawAnomaliesCatagories(){
-    $('#data-tab-second').text("")
-    let catagories = ["FastWin","MaxDamage","LongestGame"]
-    let clicks = [drawFastwinCatagory,drawFastwinCatagory,drawFastwinCatagory]
-    for(i = 0; i < catagories.length; i++){
-        let catagoryBtn = $(document.createElement('button')).attr('class','btn').text(catagories[i]).click(clicks[i])
-        $('#data-tab-second').append(catagoryBtn)
-    }
-}
 
-function drawFastwinCatagory(){
+function drawAnomaliesSection(anomaliesType){
+    let sectionID = anomaliesType +'-section'
+    let setionDiv = createSection(anomaliesType,sectionID)
+    $('#data-page').append(setionDiv)
     dbmanager.loadDB(function(){
         let currentGame = dbmanager.getCurrentGameName()
         let recordDataRoot = dbmanager.getGameRecordDataRoot(currentGame,false)
         console.log("fastwin: "+recordDataRoot)
-        $('#data-page').text("")
+        $('#'+sectionID).text("")
         let recordGroupList = $(document.createElement('ul')).attr('class','list-group')
-        $('#data-page').append(recordGroupList)
+        $('#'+sectionID).append(recordGroupList)
         for(i = 0; i < 10; i++){
             let recordItem = $(document.createElement('li')).text("record"+i).attr('class','list-group-item')
             let recordPlayBtn = $(document.createElement('button')).text("Play")
@@ -106,180 +118,5 @@ function drawFastwinCatagory(){
             recordItem.append(recordPlayBtn,reocrdTimeline)
             recordGroupList.append(recordItem)
         }
-    })
-}
-
-function showPieChart(){   
-    // clear 
-    $("#my_dataviz").text("")
-    // set the dimensions and margins of the graph
-    var width = 450
-    height = 450
-    margin = 40
-
-    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-    var radius = Math.min(width, height) / 2 - margin
-
-    // append the svg object to the div called 'my_dataviz'
-    var svg = d3.select("#my_dataviz")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    // Create dummy data
-    var data = {a: 9, b: 20, c:30, d:8, e:12}
-
-    // set the color scale
-    var color = d3.scaleOrdinal()
-    .domain(data)
-    .range(d3.schemeSet2);
-
-    // Compute the position of each group on the pie:
-    var pie = d3.pie()
-    .value(function(d) {return d.value; })
-    var data_ready = pie(d3.entries(data))
-    // Now I know that group A goes from 0 degrees to x degrees and so on.
-
-    // shape helper to build arcs:
-    var arcGenerator = d3.arc()
-    .innerRadius(0)
-    .outerRadius(radius)
-
-    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-    svg
-    .selectAll('mySlices')
-    .data(data_ready)
-    .enter()
-    .append('path')
-    .attr('d', arcGenerator)
-    .attr('fill', function(d){ return(color(d.data.key)) })
-    .attr("stroke", "black")
-    .style("stroke-width", "2px")
-    .style("opacity", 0.7)
-
-    // Now add the annotation. Use the centroid method to get the best coordinates
-    svg
-    .selectAll('mySlices')
-    .data(data_ready)
-    .enter()
-    .append('text')
-    .text(function(d){ return "grp " + d.data.key})
-    .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
-    .style("text-anchor", "middle")
-    .style("font-size", 17)
-}
-
-function showZoomHis(){
-    // clear 
-    $("#my_dataviz").text("")
-    // set the dimensions and margins of the graph
-    var width = 460
-    var height = 460
-
-    // append the svg object to the body of the page
-    var svg = d3.select("#my_dataviz")
-    .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-
-    // Read data
-    d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/11_SevCatOneNumNestedOneObsPerGroup.csv", function(data) {
-
-    // Filter a bit the data -> more than 1 million inhabitants
-    data = data.filter(function(d){ return d.value>10000000 })
-
-    // Color palette for continents?
-    var color = d3.scaleOrdinal()
-        .domain(["Asia", "Europe", "Africa", "Oceania", "Americas"])
-        .range(d3.schemeSet1);
-
-    // Size scale for countries
-    var size = d3.scaleLinear()
-        .domain([0, 1400000000])
-        .range([7,55])  // circle will be between 7 and 55 px wide
-
-    // create a tooltip
-    var Tooltip = d3.select("#my_dataviz")
-        .append("div")
-        .style("opacity", 0)
-        .attr("class", "tooltip")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "2px")
-        .style("border-radius", "5px")
-        .style("padding", "5px")
-
-    // Three function that change the tooltip when user hover / move / leave a cell
-    var mouseover = function(d) {
-        Tooltip
-        .style("opacity", 1)
-    }
-    var mousemove = function(d) {
-        Tooltip
-        .html('<u>' + d.key + '</u>' + "<br>" + d.value + " inhabitants")
-        .style("left", (d3.mouse(this)[0]+20) + "px")
-        .style("top", (d3.mouse(this)[1]) + "px")
-    }
-    var mouseleave = function(d) {
-        Tooltip
-        .style("opacity", 0)
-    }
-
-    // Initialize the circle: all located at the center of the svg area
-    var node = svg.append("g")
-        .selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("class", "node")
-        .attr("r", function(d){ return size(d.value)})
-        .attr("cx", width / 2)
-        .attr("cy", height / 2)
-        .style("fill", function(d){ return color(d.region)})
-        .style("fill-opacity", 0.8)
-        .attr("stroke", "black")
-        .style("stroke-width", 1)
-        .on("mouseover", mouseover) // What to do when hovered
-        .on("mousemove", mousemove)
-        .on("mouseleave", mouseleave)
-        .call(d3.drag() // call specific function when circle is dragged
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
-
-    // Features of the forces applied to the nodes:
-    var simulation = d3.forceSimulation()
-        .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
-        .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
-        .force("collide", d3.forceCollide().strength(.2).radius(function(d){ return (size(d.value)+3) }).iterations(1)) // Force that avoids circle overlapping
-
-    // Apply these forces to the nodes and update their positions.
-    // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
-    simulation
-        .nodes(data)
-        .on("tick", function(d){
-            node
-                .attr("cx", function(d){ return d.x; })
-                .attr("cy", function(d){ return d.y; })
-        });
-
-    // What happens when a circle is dragged?
-    function dragstarted(d) {
-        if (!d3.event.active) simulation.alphaTarget(.03).restart();
-        d.fx = d.x;
-        d.fy = d.y;
-    }
-    function dragged(d) {
-        d.fx = d3.event.x;
-        d.fy = d3.event.y;
-    }
-    function dragended(d) {
-        if (!d3.event.active) simulation.alphaTarget(.03);
-        d.fx = null;
-        d.fy = null;
-    }
-
     })
 }
--- BoardEngine/scripts/dataVisualizer.js
+++ BoardEngine/scripts/dataVisualizer.js
@@ -146,6 +146,9 @@ function drawRelationshipTable(divID){
 
     // Three function that change the tooltip when user hover / move / leave a cell
     var mouseover = function(d) {
+    let data = {a: Math.random(), b: Math.random(), c:Math.random(), d:Math.random(), e:Math.random()}
+    drawPieChart('card-relationship-pie',data)
+
     tooltip
         .style("opacity", 1)
     d3.select(this)
@@ -205,8 +208,68 @@ function drawRelationshipTable(divID){
         .text("A short description of cards relationship");
 }
 
+function drawPieChart(divID,data){   
+    // clear 
+    $("#"+divID).text("")
+    // set the dimensions and margins of the graph
+    var width = 450
+    height = 450
+    margin = 40
+
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    var radius = Math.min(width, height) / 2 - margin
+
+    // append the svg object to the div called 'my_dataviz'
+    var svg = d3.select("#"+divID)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // set the color scale
+    var color = d3.scaleOrdinal()
+    .domain(data)
+    .range(d3.schemeSet2);
+
+    // Compute the position of each group on the pie:
+    var pie = d3.pie()
+    .value(function(d) {return d.value; })
+    var data_ready = pie(d3.entries(data))
+    // Now I know that group A goes from 0 degrees to x degrees and so on.
+
+    // shape helper to build arcs:
+    var arcGenerator = d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius)
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+    .selectAll('mySlices')
+    .data(data_ready)
+    .enter()
+    .append('path')
+    .attr('d', arcGenerator)
+    .attr('fill', function(d){ return(color(d.data.key)) })
+    .attr("stroke", "black")
+    .style("stroke-width", "2px")
+    .style("opacity", 0.7)
+
+    // Now add the annotation. Use the centroid method to get the best coordinates
+    svg
+    .selectAll('mySlices')
+    .data(data_ready)
+    .enter()
+    .append('text')
+    .text(function(d){ return "grp " + d.data.key})
+    .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+    .style("text-anchor", "middle")
+    .style("font-size", 17)
+}
+
 
 module.exports ={
     drawRankChart,
-    drawRelationshipTable
+    drawRelationshipTable,
+    drawPieChart,
 }
