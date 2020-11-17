3285893b7ba536b6328e73ed7b983acaf9344a0f
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-11-17 12:33:34 -0500

    sperate playtest codes from library.js

--- BoardEngine/scripts/library.js
+++ BoardEngine/scripts/library.js
@@ -1,8 +1,9 @@
 const dbmanager = require('../scripts/dbmanager')
 const rootPath = require('electron-root-path').rootPath
-const dataVisualizer = require('../scripts/dataVisualizer')
 const cardRenderer = require('../scripts/cardRenderer')
 const utils = require('../scripts/utils')
+const pagePlaytest = require('../scripts/pagePlaytest')
+
 const PythonProcess = require('../Scripts/pythonProcess.js')
 
 var currentGameData = {}
@@ -186,68 +187,11 @@ function onClickRemoveGame(){
 }
 
 function onClickPlaytest(){
-    let gameNums = $('#playtest-game-num').val()
-    let gameName = dbmanager.getCurrentGameName()
-    let deckName = currentGameData.rules.deck
-    let trainVersion = $('#AI-dropdown-btn').text()
-
-    if(!dbmanager.getAllTrainedVersion(gameName,deckName).includes(trainVersion)){
-        popupWarning("must select correct AI to playtest!")
-        return 
-    }
-    $('#playtest-btn').addClass('d-none')
-    $('#playtest-config-div').addClass('d-none')
-    $('#playtest-progress').removeClass('d-none')
-    $('#playtest-status').removeClass('d-none')
-    $('#data-display-root').addClass('d-none')
-    console.log('Playtst: [train ver]-'+trainVersion)
-    let pyProcess = new PythonProcess(
-        12,
-        {'train_version': trainVersion,'game_nums':gameNums},
-	    function () { console.log('success!') },
-        onReceivePlaytestMesssage,
-        function(err){popupWarning(err)})
-    $('#playtest-status').text('load trained AI modal...')
-}
-
-function onReceivePlaytestMesssage(data){
-    simulateInfo = JSON.parse(data).content
-    let curprogress =   simulateInfo.curprogress
-    let maxprogress =  simulateInfo.maxprogress
-    let percentage = Math.ceil(100*curprogress/maxprogress)
-    $('#playtest-status').text('Is playtesting...')
-    $('#playtest-progress-bar').attr("class","c100 p"+percentage)
-    $('#playtest-progress-text').text(curprogress+'/'+maxprogress)
-    if(simulateInfo.is_finished){
-        let gamename= dbmanager.getCurrentGameName()
-        let deckname = currentGameData.rules.deck
-        let trainVersion = $('#AI-dropdown-btn').text()
-        dbmanager.recordPlaytestHistory(trainVersion)
-        viewPlaytestData(gamename,deckname, trainVersion)
-    }
+    pagePlaytest.onClickPlaytest()
 }
 
 function viewPlaytestData(gamename,deckname,trainVersion){
-    let playtestData = dbmanager.loadPlaytestData(gamename,deckname,trainVersion)
-    console.log(playtestData)
-    $('#playtest-btn').removeClass('d-none')
-    $('#playtest-config-div').removeClass('d-none')
-    $('#playtest-progress').addClass('d-none')
-    $('#playtest-status').addClass('d-none')
-    $('#data-display-root').removeClass('d-none')
-
-    let basicStats = playtestData.basicStats
-    // set wintrate graph
-    $('#win-rate-text').text(basicStats.win_rate*100+'%')
-    let percentage = Math.ceil(basicStats.win_rate*100)
-    $('#win-rate-progress').attr("class","green c100 p"+percentage)
-    $('#avg-game-len').text("average game length: "+basicStats.avg_game_length)
-    $('#avg-boss-hp').text("average boss hp: "+basicStats.avg_boss_hp)
-    $('#avg-player-hp').text("average player hp: "+basicStats.avg_player_hp)
-
-    // relationship heat map
-    dataVisualizer.drawRelationshipTable(playtestData.card_relationship_csv_url,'card-relationship-table')
-    dataVisualizer.drawRankChart( playtestData.card_perfromance_csv_url,'Card Name','card-data-rankChart')
+    pagePlaytest.viewPlaytestData(gamename,deckname,trainVersion)
 }
 
 function onSwitchCurrentDeck(deckName){
@@ -403,43 +347,7 @@ function updateDesignPage(){
 }
 
 function updatePlaytestPage(){
-    utils.setupDropdown(
-        'AI-dropdown-list',
-        'AI-dropdown-btn',
-        dbmanager.getAllTrainedVersion(dbmanager.getCurrentGameName(),currentGameData.rules.deck))
-    updatePlaytestHistory()
-
-    function updatePlaytestHistory(){
-        $('#playtest-history-list').text("")
-        for(let history of dbmanager.getPlaytestHistory(dbmanager.getCurrentGameName(),currentGameData.rules.deck)){
-            $('#playtest-history-list').append(createHistoryEntry(history))
-        }
-    }
-
-    function createHistoryEntry(history){
-        let entryRoot = $(document.createElement('div'))
-            .attr('class','row playtest-history-entry')
-            .css('border-radius','10px;')
-        //let deckText = $(document.createElement('span')).attr('class','col-3 text-center').text("Deck 1")
-        let trainVersionText = $(document.createElement('span')).attr('class','col-3').text(history)
-        let viewResBtn = $(document.createElement('button'))
-            .attr('class','col-2 btn btn-primary')
-            .text('View Result')
-            .click(function(){
-                viewPlaytestData('','',trainVersionText.text())
-                $('.playtest-history-entry')
-                    .css('color','')
-                    .css('font-weight','')
-                    .css('font-size','')
-                $(this).parent()
-                    .css('color','red')
-                    .css('font-weight','bold')
-                    .css('font-size','large')
-            })
-        let delBtn = $(document.createElement('button')).attr('class','col-2 btn btn-danger').text('Delete')
-        entryRoot.append(trainVersionText,viewResBtn,delBtn)
-        return entryRoot
-    }
+    pagePlaytest.updatePlaytestPage()
 }
 
 function popupWarning(message){
