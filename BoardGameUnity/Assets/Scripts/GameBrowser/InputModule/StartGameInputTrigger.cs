6fa68b1e11f94eb0259f3230270fd46755f2d9fc
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-12-01 17:55:53 -0500

    fix bug of reply button

--- BoardGameUnity/Assets/Scripts/GameBrowser/InputModule/StartGameInputTrigger.cs
+++ BoardGameUnity/Assets/Scripts/GameBrowser/InputModule/StartGameInputTrigger.cs
@@ -5,11 +5,9 @@ using UnityEngine.UI;
 
 namespace GameBrowser {
     public class StartGameInputTrigger : UserInputTrigger {
-        [SerializeField]
-        private Toggle enableRLBotToggle;
         public override void TriggerUserInput() {
             GameBrowser.Instance.sceneReference.loadingSign.SetActive(true);
-            GameBrowser.Instance.userInputManager.RegisterResetGameAction(enableRLBotToggle.isOn);
+            GameBrowser.Instance.userInputManager.RegisterResetGameAction(false);
         }
     }
 }
