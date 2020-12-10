063b71856e608078d985510393a5aa0d9aea3ef8
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-12-10 15:38:22 -0500

    add explaination txt

--- BoardEngine/scripts/explanation.js
+++ BoardEngine/scripts/explanation.js
@@ -9,16 +9,19 @@ const endingMargin =  ['This graph show the distributon of how many hp of player
 '(PS: this probalility density is estimated by treating the hp left during playtest as random varibale)']
 
 
-const cardUtil = ['card uilizations:','']
+const cardUtil = ['card uilizations:','The value represent the probability of if AI will play this card, when this card is on the hand']
 
-const cardPlayPos = ['card playpos:','']
+const cardPlayPos = ['card play position:',
+'The value represent at which stage the card will be played in average',
+'Small value means it will play early in one turn']
 
-const cardPlayCount = ['card playcount:','']
+const cardPlayCount = ['card play count:',
+'The value represent how many times the card is played during the playtest']
 
-const anomalies = ['anomalies:','']
+const anomalies = ['anomalies:','We record some games played during the playtest and visualize the process']
 
-const pairsRelationship = ['pairs:','']
+const pairsRelationship = ['pairs:','Heatmap of card-to-card relationship pairs','More red the block is, more frequent that pair is']
 
-const triplesRelationship = ['triple:','']
+const triplesRelationship = ['triple:','Top 5 triple of cards which played most frequently']
 
 module.exports ={gameLen,endingMargin,cardUtil,cardPlayPos,cardPlayCount,anomalies,pairsRelationship,triplesRelationship}
